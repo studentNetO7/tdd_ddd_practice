596b1e5fadb3b45939c36ce7e9b395e1b866fd2b
Olga7 test <7enovaoa@gmail.com>
2024-11-19 21:58:54 +0000

    printAllNames implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,14 +1,18 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class PhoneBook {
     private Map<String, String> contactsNameToNumber = new HashMap<>();
     private Map<String, String> contactsNumberToName = new HashMap<>();
+    private Set<String> names = new TreeSet<>();
 
     public int add( String name, String number){
         if (!contactsNameToNumber.containsKey(name)){
             contactsNameToNumber.put(name, number);
-            contactsNumberToName.put(number,name);}
+            contactsNumberToName.put(number,name);
+            names.add(name);}
         return contactsNameToNumber.size();
     }
 
@@ -20,7 +24,10 @@ public class PhoneBook {
         return contactsNameToNumber.get(name); // Быстрый поиск по номеру
     }
     public String printAllNames(){
-
-        return "";
+        StringBuilder sb = new StringBuilder();
+        for (String name : names){
+           sb.append(name).append("\n");
+        }
+        return sb.toString();
     }
 }
