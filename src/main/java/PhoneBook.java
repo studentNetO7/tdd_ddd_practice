b1345929bbaee69b6ac11a08f56e166af40112af
Olga7 test <7enovaoa@gmail.com>
2024-11-19 21:25:30 +0000

    findByNumber implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -2,14 +2,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PhoneBook {
-    private Map<String, String> contacts = new HashMap<>();
+    private Map<String, String> contactsNameToNumber = new HashMap<>();
+    private Map<String, String> contactsNumberToName = new HashMap<>();
 
     public int add( String name, String number){
-        if (!contacts.containsKey(name)){
-        contacts.put(name, number);}
-        return contacts.size();
+        if (!contactsNameToNumber.containsKey(name)){
+            contactsNameToNumber.put(name, number);
+            contactsNumberToName.put(number,name);}
+        return contactsNameToNumber.size();
     }
-    public String findByNumber (String number){
-        return null;
-    }
+
+    public String findByNumber(String number) {
+            return contactsNumberToName.get(number); // Быстрый поиск по номеру
+        }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -8,7 +8,7 @@ public class PhoneBookTest {
     public void testAdd (){
         PhoneBook phoneBook = new PhoneBook();
         // Добавляем первый контакт и проверяем, что количество контактов стало 1
-        assertEquals(1, phoneBook.add("Olga", "445566112233"));
+        assertEquals(1, phoneBook.add("Olga", "445"));
 
         // Добавляем второй контакт и проверяем, что количество контактов стало 2
         assertEquals(2, phoneBook.add("Lev", "123456789"));
@@ -19,15 +19,15 @@ public class PhoneBookTest {
     @Test
     public void testFindByNumber () {
         PhoneBook phoneBook = new PhoneBook();
+       // Добавляем контакты
+        phoneBook.add("Olga", "445566112233");
+        phoneBook.add("Lev", "123456789");
+        phoneBook.add("Phillip", "555666777");
        // Проверяем случаи когда номера записаны в телефонную книгу
         assertEquals("Olga", phoneBook.findByNumber("445566112233"));
         assertEquals("Lev", phoneBook.findByNumber("123456789"));
         assertEquals("Phillip", phoneBook.findByNumber("555666777"));
         // Проверяем случай когда номер не записан в телефонную книгу
         assertNull(phoneBook.findByNumber("000000000000"));
-
-
-
-
     }
 }
