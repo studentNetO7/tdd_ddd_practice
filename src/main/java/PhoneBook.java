2a8f4d5e0683dd357ef468ca1335ccd3ffa60f9f
Olga7 test <7enovaoa@gmail.com>
2024-11-19 17:13:00 +0000

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -4,7 +4,8 @@ import java.util.Map;
 public class PhoneBook {
     private Map<String, String> contacts = new HashMap<>();
     public int add( String name, String number){
-
-        return 0;
+        if (!contacts.containsKey(name)){
+        contacts.put(name, number);}
+        return contacts.size();
     }
 }
