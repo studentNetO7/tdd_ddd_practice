7cf52ba4c1ee7234aa0a8c92bc2068f4dd53c566
Olga7 test <7enovaoa@gmail.com>
2024-11-19 17:10:17 +0000

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,10 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    private Map<String, String> contacts = new HashMap<>();
+    public int add( String name, String number){
+
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PhoneBookTest {
+    @Test
+    public void testAdd (){
+PhoneBook phoneBook = new PhoneBook();
+        // Добавляем первый контакт и проверяем, что количество контактов стало 1
+        assertEquals(1, phoneBook.add("Olga", "445566112233"));
+
+        // Добавляем второй контакт и проверяем, что количество контактов стало 2
+        assertEquals(2, phoneBook.add("Lev", "123456789"));
+
+        // Добавляем третий контакт и проверяем, что количество контактов стало 3
+        assertEquals(3, phoneBook.add("Phillip", "555666777"));
+    }
 }
